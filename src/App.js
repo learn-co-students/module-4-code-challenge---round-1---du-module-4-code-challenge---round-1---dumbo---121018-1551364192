24626a8f6de499a8f21118b6e3f0cb072ab0f2c7
Jarrianm <jarrian.mclean@gmail.com>
2019-02-28 09:43:29 -0500

    completed Fetch

--- src/App.js
+++ src/App.js
@@ -5,7 +5,21 @@ import BookList from "./containers/BookList";
 import Bookshelf from "./containers/Bookshelf";
 
 class App extends Component {
+
+  state = {
+    books: []
+  }
+  componentDidMount(){
+    fetch("http://localhost:3005/books")
+    .then(res => res.json())
+    .then(books => this.setState({
+      books: books
+    }))
+  }
+  // fetch complete 1
   render() {
+    // check for fetch value
+    console.log(this)
     return (
       <div className="book-container">
         <BookList />
