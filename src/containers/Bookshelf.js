6e30992744f4c6a0c6ca7886d0cdbc259de38fbc
Jarrianm <jarrian.mclean@gmail.com>
2019-02-28 10:35:55 -0500

    successfully added list to shelves

--- src/containers/Bookshelf.js
+++ src/containers/Bookshelf.js
@@ -3,11 +3,14 @@ import Book from "../components/Book";
 
 const Bookshelf = (props) => {
   console.log(props)
-  // let book = props.
+  let book = props.clickedBook.map((book) =>{
+    console.log(book)
+    return <Book key={book.id}title={book.title} img={book.img}/>
+  })
   return (
     <div>
       <h1>Book Shelf</h1>
-      <ul>{}</ul>
+      <ul>{book}</ul>
     </div>
   );
 };
