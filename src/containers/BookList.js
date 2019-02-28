2a871bb099aebb01b5270853340ab6c52f05baf5
Joshua Alphonse <joshalphonse@gmail.com>
2019-02-28 10:17:07 -0500

    try render

--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -2,23 +2,14 @@ import React, { Component } from "react";
 import Book from "../components/Book";
 import Search from "../components/Search";
 
+const BookList = (props) => {
+  let bookList = props.books.map(book => <Book key={book.id}/>)
+  return (
+    <div className="book-list">
+      <h1>Book List</h1>
+      <ul>{bookList}</ul>
+    </div>
+  );
+};
 
-class App extends Component {
-
-
-
-
-  render() {
-    let bookList = this.props.books.map(book => <Book key={book.id}/>);
-
-    return (
-
-        <div className="book-list">
-          <h1>Book List</h1>
-          <ul>{bookList}</ul>
-        </div>
-      );
-  }
-}
-
-export default App;
+export default BookList;
